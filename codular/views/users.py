f74351f097acd7024a0f558454946b068111f426
Damien Nicolas <git@gordon.re>
2014-01-12 22:15:52 +0100

    Merged views/user.py in views/users.py

--- codular/views/users.py
+++ codular/views/users.py
@@ -3,6 +3,12 @@ Users view
 """
 from pyramid.response import Response
 from pyramid.view import ( view_config, view_defaults )
+from pyramid.security import authenticated_userid
+from pyramid.httpexceptions import HTTPFound
+
+from deform import Form
+from deform.exception import ValidationFailure
+from ..forms import Registration
 
 from sqlalchemy.exc import DBAPIError
 
@@ -34,9 +40,35 @@ class ViewUsers(object):
         template_vars = {'menu_active': 'user'}
         return template_vars
 
-    @view_config(route_name='update_user', renderer='json')
+    @view_config(route_name='update_user', request_method='POST', renderer='users/new.jinja2')
     def update_user(self):
         template_vars = {'menu_active': 'update_user'}
+        form = Form(Registration().bind(request=self.request), buttons=('submit',),
+                action=self.request.route_url('update_user'))
+        if 'submit' in self.request.POST: # detect that the submit button was clicked
+
+            controls = self.request.POST.items() # get the form controls
+
+            try:
+                form_data = form.validate(controls)  # call validate
+            except ValidationFailure as e: # catch the exception
+                template_vars['form'] = form.render() # re-render the form with an exception
+                return template_vars
+
+            # if user is logged, we update his profile. Else we create a new user.
+            userid = authenticated_userid(self.request)
+            if userid:
+                user =  User.by_id(userid)
+                # WARNING: only update authorized values
+
+            else:
+                del(form_data['csrf'])
+                user = User(**form_data)
+                DBSession.add(user)
+
+            url = self.request.route_url('home')
+            return HTTPFound(location=url)
+        template_vars['form'] = form.render()
         return template_vars
 
     @view_config(route_name='delete_user', renderer='json')
@@ -45,8 +77,12 @@ class ViewUsers(object):
         return template_vars
 
     @view_config(route_name='new_user', renderer='users/new.jinja2')
+    @view_config(route_name='register', renderer='users/new.jinja2')
     def new_user(self):
         template_vars = {'menu_active': 'new_user'}
+        form = Form(Registration().bind(request=self.request), buttons=('submit',),
+                action=self.request.route_url('update_user'))
+        template_vars['form'] = form.render()
         return template_vars
 
     @view_config(route_name='edit_user', renderer='json')
